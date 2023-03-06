5be559e7dcae80532be90d02762539f93c7b506e
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-03-06 00:54:13 -0300

    Add files via upload

---
+++ 1 Descubra numero Maior e Menor.py
@@ -0,0 +1,18 @@
+
+
+
+a = int(input("Digite o primeiro valor:"))
+b = int(input("Digite o segundo valor:"))
+c = int(input("Digite o terceiro valor:"))
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+menor = a
+if b < c and b < a:
+    menor = b
+if c < b and c < a:
+    menor = c
+print(f"\nO menor número digitado foi {menor}")
+print(f"\nO maior número digitado foi {maior}")
